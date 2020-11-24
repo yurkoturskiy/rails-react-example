2f0509395ef1de9cef85201947cda1497789df64
guandjoy <guandjoy@gmail.com>
2020-11-24 11:40:25 +0200

    Install graphql and playground and create route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  if Rails.env.development?
+    mount GraphqlPlayground::Rails::Engine, at: "/playground", graphql_path: "/graphql"
+  end
+  post "/graphql", to: "graphql#execute"
   root 'application#frontend'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
